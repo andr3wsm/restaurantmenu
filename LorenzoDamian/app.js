ee5c5f961f4c37b7d22138e51a8a3f4a17c38113
LorenzoDamian <lorenzo.damian03@gmail.com>
2019-11-19 19:21:07 +0100

    starting problematico

---
+++ LorenzoDamian/app.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+
+//import routes from './routes/index.js';
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// catch 400
+app.use((err, req, res, next) => {
+    console.log(err.stack);
+    res.status(400).send(`Error: ${res.originUrl} not found`);
+    next();
+});
+
+// catch 500
+app.use((err, req, res, next) => {
+    console.log(err.stack)
+    res.status(500).send(`Error: ${err}`);
+    next();
+});
+
+routes(app);
+
+export default app;
